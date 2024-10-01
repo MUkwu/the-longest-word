e0fc7223c78ae9acac5c56412c022be8b321d208
Mez Ukwuoma <mezdylan@hotmail.com>
2024-10-01 19:05:31 +0000

    Game development with TDD

---
+++ the_longest_word/game.py
@@ -0,0 +1,20 @@
+import random
+
+class Game:
+    def __init__(self) -> list:
+        """Attribute a random grid to size 9"""
+        self.grid = None # TODO
+ 
+        alpha = 'abcdefghijklmnopqrstuvwxyz'
+        i = 0
+        nine_letters = []
+        while i < 9:
+            position = random.randrange(0,25)
+            nine_letters.append(alpha[position])
+            i = i + 1
+
+        pass
+
+    def is_valid(self, word: str) -> bool:
+        """Return True if and only if the word is valid, given the Game's grid"""
+        pass # TODOpoetry run
